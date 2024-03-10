52a3e529b416edd2db9da78bd50e9ffdf61ab67f
Mohamed <mohammedmohamuud.01@gmail.com>
2024-03-10 21:37:32 +0300

    Created the important files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import React from "react";
+import styles from "./style";
+import {
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+  NavBar,
+  Hero,
+} from "./Components";
 
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <h1>Hello World</h1>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <NavBar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />{" "}
+          <Clients /> <CTA /> <Footer />
+        </div>
+      </div>
     </div>
   );
 };
