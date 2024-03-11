5d58a9e91a3d11be8803ae3d51e297f47db949f5
Mohamed <mohammedmohamuud.01@gmail.com>
2024-03-11 21:31:30 +0300

    Build the navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../Constants";
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
